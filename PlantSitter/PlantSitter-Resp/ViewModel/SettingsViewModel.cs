640a2d9b84cd905c5aa7855715d043036a9ab870
juniperphoton <dengweichao@hotmail.com>
2016-04-02 15:55:45 +0800

    Add app settings.

---
+++ PlantSitter/PlantSitter-Resp/ViewModel/SettingsViewModel.cs
@@ -0,0 +1,32 @@
+﻿using GalaSoft.MvvmLight;
+using JP.Utils.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlantSitter_Resp.ViewModel
+{
+    public class SettingsViewModel : ViewModelBase, INavigable
+    {
+        public bool IsFirstActived { get; set; }
+
+        public bool IsInView { get; set; }
+
+        public void Activate(object param)
+        {
+             
+        }
+
+        public void Deactivate(object param)
+        {
+             
+        }
+
+        public void OnLoaded()
+        {
+             
+        }
+    }
+}
---
+++ PlantSitter/PlantSitterShared/Common/AppSettings.cs
@@ -0,0 +1,39 @@
+﻿
+using GalaSoft.MvvmLight;
+using System;
+using Windows.Storage;
+
+namespace PlantSitterShared.Common
+{
+    public class AppSettings : ViewModelBase
+    {
+        public ApplicationDataContainer LocalSettings { get; set; }
+
+        public AppSettings()
+        {
+            LocalSettings = ApplicationData.Current.LocalSettings;
+        }
+
+        protected void SaveSettings(string key, object value)
+        {
+            LocalSettings.Values[key] = value;
+        }
+
+        protected T ReadSettings<T>(string key, T defaultValue)
+        {
+            if (LocalSettings.Values.ContainsKey(key))
+            {
+                return (T)LocalSettings.Values[key];
+            }
+            if (defaultValue != null)
+            {
+                return defaultValue;
+            }
+            return default(T);
+        }
+
+        private static readonly Lazy<AppSettings> lazy = new Lazy<AppSettings>(() => new AppSettings());
+
+        public static AppSettings Instance { get { return lazy.Value; } }
+    }
+}
