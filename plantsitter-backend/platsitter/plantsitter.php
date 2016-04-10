5f487513bcd396fa21b0a33d8abfe729185587e9
juniperphoton <dengweichao@hotmail.com>
2016-04-11 01:00:30 +0800

    Update some backend files. Now can read DTH11 sensor.

---
+++ plantsitter-backend/platsitter/plantsitter.php
@@ -0,0 +1,19 @@
+<?php
+
+$ApiAction = array(
+    'Register',
+    'Login',
+    'AddNewSchedule',
+    'UpdateSchedule',
+    'DeleteSchedule',
+    'DoneSchedule',
+    'SyncSchedules'
+);
+
+do
+{
+	echo "test ok";
+	break;
+}while (0)
+
+?>
